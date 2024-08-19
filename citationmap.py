67f6706323be0d5013f99f35d4dcb8fe041e6f01
vztu <tzz.fdu@gmail.com>
2024-08-19 16:04:29 -0500

    init

---
+++ citationmap.py
@@ -0,0 +1,6 @@
+from citation_map import generate_citation_map
+
+if __name__ == '__main__':
+    # This is my Google Scholar ID. Replace this with your ID.
+    scholar_id = '9ajdZaEAAAAJ'
+    generate_citation_map(scholar_id)
